c8d28c07e74bc2dbc192f6da72944149f7e8256c
Thiago pedoni <tpedoni@fi.uba.ar>
2024-11-06 22:43:37 -0300

    añado base.html y modificación del index.html

---
+++ Frontend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run("127.0.0.1", port="5000", debug=True)
